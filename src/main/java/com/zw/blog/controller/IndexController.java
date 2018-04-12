563c8011e4557f6a41bc18b218af7aa07c1cb302
HuangJian <173247429@qq.com>
2018-04-12 15:24:26 +0800

    Swagger2整合
    Signed-off-by: HuangJian <173247429@qq.com>

--- src/main/java/com/zw/blog/controller/IndexController.java
+++ src/main/java/com/zw/blog/controller/IndexController.java
@@ -1,9 +1,11 @@
 package com.zw.blog.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author gan zw
@@ -15,7 +17,8 @@ public class IndexController {
 
     private Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-    @RequestMapping(value = {"/", "index"})
+    @ApiOperation(value="获取图书列表", notes="获取图书列表")
+    @RequestMapping(value = {"/", "index"},method= RequestMethod.GET)
     public String index() {
         return "index";
     }
